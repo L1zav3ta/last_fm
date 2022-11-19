31423c6e1acf969874a95fce2eb476eb581792f2
Elizaveta Medvedeva <liza01700@gmail.com>
2022-11-19 21:08:38 +0300

    Диплом. JS

---
+++ public/modules/const.js
@@ -0,0 +1,7 @@
+export const API_KEY = '617e499ad5ba1abf497871f591effb53';
+export const API_URL = 'https://ws.audioscrobbler.com/2.0/';
+
+export const MAX_ARTISTS = 12;
+export const MAX_TRACKS = 15;
+
+export const SEARCH_LIMIT = 8;
